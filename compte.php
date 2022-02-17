7f8779fc809020b386b5450824df0aa725c9f346
Elyroma <95436804+Elyroma@users.noreply.github.com>
2022-02-17 11:35:06 +0100

    debug

--- compte.php
+++ compte.php
@@ -1,23 +1,23 @@
 <?php
 function Compte(){
   ?>
-
-
-<nav>
-  <ul>
-    <li><a href="?page=compte&pageCompte=modifInfos">Modifier mes informations</a></li>
-    <li><a href="?page=compte&pageCompte=modifMDP">Modifier mon mot de passe</a></li>
-    <li><a href="?page=compte&pageCompte=voirRecommandations">Voir mes recommandations</a></li>
-    <li><a href="?page=compte&pageCompte=ajoutRecommandation">Ajouter une recommandation</a></li>
-    <li><a href="?page=compte&pageCompte=voirAnnonces">Voir mes annonces</a></li>
-    <li><a href="?page=compte&pageCompte=ajoutAnnonce">Ajouter une annonce</a></li>
-  </ul>
-</nav>
+  <nav>
+    <ul>
+      <li><a href="?page=compte&pageCompte=modifInfos">Modifier mes informations</a></li>
+      <li><a href="?page=compte&pageCompte=modifMDP">Modifier mon mot de passe</a></li>
+      <li><a href="?page=compte&pageCompte=voirRecommandations">Voir mes recommandations</a></li>
+      <li><a href="?page=compte&pageCompte=ajoutRecommandation">Ajouter une recommandation</a></li>
+      <li><a href="?page=compte&pageCompte=voirAnnonces">Voir mes annonces</a></li>
+      <li><a href="?page=compte&pageCompte=ajoutAnnonce">Ajouter une annonce</a></li>
+    </ul>
+  </nav>
 
 <?php 
-if ($pageCompte = "home"){
-  echo "page home du compte";
-} elseif ($pageCompte == "ajoutAnnonce"){
-  addLogement();
-}
+  if ($pageCompte = "home"){
+    echo "page home du compte";
+  } elseif ($pageCompte == "ajoutAnnonce"){
+    addLogement();
+  }
+  var_dump ($pageCompte);
+  
 } ?>
