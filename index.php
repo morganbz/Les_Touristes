35049fb2adfbdfdbded1e29acc948734040309e5
Elyroma <95436804+Elyroma@users.noreply.github.com>
2022-02-17 09:52:31 +0100

    recuperation index et view

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "constant.php";
+
+include_once "db.php";
+
+include_once "function/function_db.php";
+
+include_once "pages/compte.php";
+
+include_once "action.php";
+
+include_once "view.php";
+
+?>
---
+++ view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les Touristes</title>
+</head>
+<body>
+    <header>
+        <marquee><h1>Salut les touristes !</h1></marquee>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="?page=compte">Compte</a></li>
+            <li><a href="">Ajouter un logement</a></li>
+            <li><a href="">Rechercher un logement</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
