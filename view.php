62fa954572427f70461940181a8365b65443c9c2
Elyroma <elynebeaudeau@orange.fr>
2022-03-06 19:52:35 +0100

    \n

--- view.php
+++ view.php
@@ -59,7 +59,7 @@
         }
 
       if (isset($_SESSION["errors_register"])){
-        echo "Erreurs lors de la création de compte :";
+        echo "\nErreurs lors de la création de compte :";
         echo "<ul>";
         foreach($_SESSION["errors_register"] as $error_register)
             echo "<li class='error'>$error_register</li>";
