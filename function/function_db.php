a40d2a8265f8009a2a9d473177a27810b0df02b1
Elyroma <elynebeaudeau@orange.fr>
2022-03-06 20:29:03 +0100

    debug

--- function/function_db.php
+++ function/function_db.php
@@ -32,7 +32,7 @@ function addUser($mail, $firstname, $lastname, $birth_date, $phone, $password, $
                 $id = mysqli_fetch_assoc($result);
                 $_SESSION["id_user"] = $id;
         } else {
-                $errors[] = "Erreur au moment de l'ajout dans la base de donnée" + $insert_user + $insert_user_info;
+                $errors[] = "Erreur au moment de l'ajout dans la base de donnée" + var_dump($insert_user);
                 $_SESSION["errors_register"] = $errors;
         }
 }
