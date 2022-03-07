15eefa5637de8c24071525c26b6435ad2de73267
Elyroma <elynebeaudeau@orange.fr>
2022-03-07 16:07:34 +0100

    ;

--- view.php
+++ view.php
@@ -63,7 +63,7 @@
             include_once "login.php";
         }
         else if ($page == "deconnexion"){
-            $page = "home"
+            $page = "home";
             session_unset();
         }
         else if($page == "search_housing"){
