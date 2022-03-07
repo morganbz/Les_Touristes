e44ca16e4a48fd7b7ee19cd4e5a89bf42a66a0bc
Elyroma <elynebeaudeau@orange.fr>
2022-03-07 14:44:49 +0100

    plus propre

--- modif_infos_user.php
+++ modif_infos_user.php
@@ -1,12 +1,5 @@
 <?php
-     $firstname = "Prénom";
-     $lastname = "Nom";
-     $email = "Email";
-     $birth_date = "Date de naissance";
-     $phone = "Numéro de téléphone";
-     $description = "Description";
-
-     $affichage = "placeholder";
+     $description = "";
     
     if(isset($_SESSION["id_user"])){
         $user = getUserById($_SESSION["id_user"]);
@@ -19,39 +12,38 @@
         if ($user["description"] != NULL){
             $description = $user["description"];
         }
-        $affichage = "value";
     }
 ?>
 
 <form action="index.php" method="post">
     <div>
         <label for="firstname_modification">Prénom</label>
-        <input <?php echo $affichage;?>="<?php echo $firstname;?>" type="text" name="firstname_modification" id="firstname_modification" required>
+        <input placeholder="Prénom" value="<?php echo $firstname;?>" type="text" name="firstname_modification" id="firstname_modification" required>
     </div>
 
     <div>
         <label for="lastname_modification">Nom</label>
-        <input <?php echo $affichage;?>="<?php echo $lastname;?>" type="text" name="lastname_modification" id="lastname_modification" required>
+        <input placeholder="Nom" value="<?php echo $lastname;?>" type="text" name="lastname_modification" id="lastname_modification" required>
     </div>
 
     <div>
         <label for="email_modification">Email</label>
-        <input <?php echo $affichage;?>="<?php echo $email;?>" type="email" name="email_modification" id="email_modification" required>
+        <input placeholder="exemple@mail.com" value="<?php echo $email;?>" type="email" name="email_modification" id="email_modification" required>
     </div>
 
     <div>
         <label for="birth_date_modification">Date de naissance</label>
-        <input <?php echo $affichage;?>="<?php echo $birth_date;?>" type="date" name="birth_date_modification" id="birth_date_modification" required>
+        <input placeholder="Date de naissance" value="<?php echo $birth_date;?>" type="date" name="birth_date_modification" id="birth_date_modification" required>
     </div>
 
     <div>
         <label for="phone_modification">Numéro de Téléphone</label>
-        <input <?php echo $affichage;?>="<?php echo $phone;?>" type="text" name="phone_modification" id="phone_modification" required>
+        <input placeholder="Numéro de téléphone" value="<?php echo $phone;?>" type="text" name="phone_modification" id="phone_modification" required>
     </div>
 
     <div>
         <label for="description_modification">Description</label>
-        <textarea <?php echo $affichage;?>="<?php echo $description;?>" name="description_modification" id="description_modification"></textarea>
+        <textarea placeholder="Description" value="<?php echo $description;?>" name="description_modification" id="description_modification"></textarea>
     </div>
 
     <button id="submit" name="submit" value="update_user_info" type="submit">Mettre à jour</button>
