b61e10f37fb2920005ca2437578aa71fbf889c19
Samuel <samwyckaert@hotmail.fr>
2022-03-09 22:16:18 +0100

    maj choix recherche

--- formulaire/search_housing_text.php
+++ formulaire/search_housing_text.php
@@ -19,5 +19,7 @@
         <label for="price_announce">Prix maximum</label>
     </div>
 
+    
+
     <button id="submit" name="submit" value="search_housing_text" type="submit">Rechercher</button>
 </form>
--- view.php
+++ view.php
@@ -14,9 +14,11 @@
     <nav>
         <ul>
             <li><a href="?page=recherche_activitee">Rechercher une activitée</a></li>
-            <li><a href="?page=search_housing_text">Rechercher un logement texte</a></li>
-            <li><a href="?page=search_housing">Recherche logement carte</a></li>
-            <li><a href="?page=test">TEST GOOGLE</a></li>
+            <?php
+            //<li><a href="?page=search_housing_text">Rechercher un logement texte</a></li>
+            //<li><a href="?page=test">TEST GOOGLE</a></li>
+            ?>
+            <li><a href="?page=search_housing">Recherche logement</a></li>
             <?php 
                 if (isset($_SESSION["id_user"])){?>
                     <li><a href="?page=user_page">Compte</a></li>
