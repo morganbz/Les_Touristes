8fd3420a657a2bf7d6c0d9a73517572c55df29ad
Sanktuss <bertcle@free.fr>
2022-03-10 11:27:55 +0100

    debug

--- function/function_db.php
+++ function/function_db.php
@@ -433,6 +433,7 @@ function addHousingAnnounceDate($id, $price, $date) {
         $date = mysqli_real_escape_string($base, $date);
 
         $sql = "INSERT INTO announce (date, price, isTaken, id_housing) VALUES ('$date', $prix, 0, $id)" ;
+        echo $sql;
 
         $insert_add_housing_announce_date = $base->query($sql);
 
