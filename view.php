acddbc2308809fa69ce7dd2afff20e1dfa53caf7
Elyroma <elynebeaudeau@orange.fr>
2022-04-28 14:19:31 +0200

    Home page

--- view.php
+++ view.php
@@ -50,7 +50,7 @@
 
             <?php
             if ($page == "home"){
-                
+                include_once "home.php";
             }
             else if ($page == "recherche_activitee"){
                 include_once "test_calendar.php";
