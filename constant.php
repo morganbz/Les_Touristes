df9f440a8251ea46f9902b85c4e4d010fc706641
Elyroma <elynebeaudeau@orange.fr>
2022-05-12 12:25:30 +0200

    debug date

--- constant.php
+++ constant.php
@@ -7,5 +7,5 @@ $TYPE_RATED = array(1 => "Logement", 2 => "Activités", 3 => "Utilisateur");
 
 var_dump(setlocale(LC_TIME, 'fr_FR.utf8','fra'));
 date_default_timezone_set('Europe/Paris');
-
+var_dump(phpinfo());
 ?>
