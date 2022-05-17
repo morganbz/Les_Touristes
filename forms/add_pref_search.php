def677185a185fcfa41d534c1dd6bb162833c775
Elyroma <elynebeaudeau@orange.fr>
2022-05-17 12:15:28 +0200

    style form preference recherche

--- forms/add_pref_search.php
+++ forms/add_pref_search.php
@@ -49,56 +49,63 @@ if(!empty($preferences)){
 
     <?php
 }
+//<div ">
 ?>
 
-<h4>Ajouter une préférence</h4>
+<div class="container h-100" style="width:60%;">
+    <div class="align-items-center m-3 display-form-bg">
+        <div class="col-lg-15" >
+            <div class="about-text go-to">
+                <h4 class="dark-color h2"><strong>Ajouter une préférence</strong></h4> 
 
-<div class="container h-100" style="width:40%;">
-    <form action="index.php" method="post">
+                <form action="index.php" method="post">
 
-        <div class="col mb-3">
-            <div class="form-floating">
-                <input placeholder="Vacances à la mer" class="form-control" type="text" name="name_pref_search" id="name_pref_search" required>
-                <label for="name_pref_search" class="form-label">Nom de la preference</label>
+                    <div class="col mb-3">
+                        <div class="form-floating">
+                            <input placeholder="Vacances à la mer" class="form-control" type="text" name="name_pref_search" id="name_pref_search" required>
+                            <label for="name_pref_search" class="form-label">Nom de la preference</label>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+
+                        <div class="col">
+                            <div class="form-floating">
+                                <input placeholder="Marseille" type="text" class="form-control" name="dest_pref_search" id="dest_pref_search" required>
+                                <label for="dest_pref_search" class="form-label">Destination</label>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-floating">
+                                <input placeholder="50" type="float" class="form-control" name="distance_pref_search" id="distance_pref_search" required>
+                                <label for="distance_pref_search" class="form-label">distance maximal (en km)</label>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="row mb-3">
+
+                        <div class="col">
+                            <div class="form-floating">
+                                <input placeholder="50" type="float" class="form-control" name="price_min_pref_search" id="price_min_pref_search" required>
+                                <label for="price_min_pref_search" class="form-label">prix minimum à la nuit (en €)</label>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-floating">
+                                <input placeholder="150" type="float" class="form-control" name="price_max_pref_search" id="price_max_pref_search" required>
+                                <label for="price_max_pref_search" class="form-label">prix maximum à la nuit (en €)</label>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <button class="btn btn-outline-primary" id="submit" name="submit" value="add_pref_search" type="submit" class="w-100 btn btn-primary btn-lg px-4 me-sm-3">Ajouter la préférence</button>
+                </form>
             </div>
         </div>
-
-        <div class="row mb-3">
-
-            <div class="col">
-                <div class="form-floating">
-                    <input placeholder="Marseille" type="text" class="form-control" name="dest_pref_search" id="dest_pref_search" required>
-                    <label for="dest_pref_search" class="form-label">Destination</label>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="form-floating">
-                    <input placeholder="50" type="float" class="form-control" name="distance_pref_search" id="distance_pref_search" required>
-                    <label for="distance_pref_search" class="form-label">distance maximal (en km)</label>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="row mb-3">
-
-            <div class="col">
-                <div class="form-floating">
-                    <input placeholder="50" type="float" class="form-control" name="price_min_pref_search" id="price_min_pref_search" required>
-                    <label for="price_min_pref_search" class="form-label">prix minimum à la nuit (en €)</label>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="form-floating">
-                    <input placeholder="150" type="float" class="form-control" name="price_max_pref_search" id="price_max_pref_search" required>
-                    <label for="price_max_pref_search" class="form-label">prix maximum à la nuit (en €)</label>
-                </div>
-            </div>
-
-        </div>
-
-        <button class="btn btn-outline-primary" id="submit" name="submit" value="add_pref_search" type="submit" class="w-100 btn btn-primary btn-lg px-4 me-sm-3">Ajouter la préférence</button>
-    </form>
+    </div>
 </div>
