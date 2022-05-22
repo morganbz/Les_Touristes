b00f4b888539d5a50ca37e3d112e02efbd38c94f
Samuel <samwyckaert@hotmail.fr>
2022-05-22 14:21:35 +0200

    ù

--- account/see_resa.php
+++ account/see_resa.php
@@ -33,56 +33,56 @@ $housings = getHousingByIdOwner($id_owner);
                 ?>
                 </div>
 
-            <div>
-                <?php
-                if($hasBook){
-                    $reservations = getAllBookByIdHousing($housing["id"]);
-                    $cpt = 1;
+                <div>
+                    <?php
+                    if($hasBook){
+                        $reservations = getAllBookByIdHousing($housing["id"]);
+                        $cpt = 1;
 
-                    ?>
-                     <table class="table table-bordered caption-top">
-                        <thead class="tableau_couleur">
-                            <tr class="text-center">
-                            <th scope="col">#</th>
-                            <th scope="col">Début du séjour</th>
-                            <th scope="col">Fin du séjour</th>
-                            <th scope="col">Utilisateur</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            foreach($reservations as $reservation){
-                                $user = getUserById($reservation["id_user"]);
-                                ?>
-                                <tr>
-                                    <th scope="row"><?php echo $cpt; ?></th>
-                                    <td><?php echo getNiceDate($reservation["date_start"]); ?></td>
-                                    <td><?php echo getNiceDate($reservation["date_end"]); ?></td>
-                                    <td><a class = "lien_logement" href="?page=user&u=<?php echo $reservation["id_user"]; ?>"><?php echo $user["mail"]; ?></a></td>
+                        ?>
+                        <table class="table table-bordered caption-top">
+                            <thead class="tableau_couleur">
+                                <tr class="text-center">
+                                <th scope="col">#</th>
+                                <th scope="col">Début du séjour</th>
+                                <th scope="col">Fin du séjour</th>
+                                <th scope="col">Utilisateur</th>
                                 </tr>
-                            <?php
-                            $cpt++;
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                    <?php
+                            </thead>
+                            <tbody>
+                                <?php
+                                foreach($reservations as $reservation){
+                                    $user = getUserById($reservation["id_user"]);
+                                    ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $cpt; ?></th>
+                                        <td><?php echo getNiceDate($reservation["date_start"]); ?></td>
+                                        <td><?php echo getNiceDate($reservation["date_end"]); ?></td>
+                                        <td><a class = "lien_logement" href="?page=user&u=<?php echo $reservation["id_user"]; ?>"><?php echo $user["mail"]; ?></a></td>
+                                    </tr>
+                                <?php
+                                $cpt++;
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                        <?php
 
-                }
-                else{
+                    }
+                    else{
+                        ?>
+                        <p>Il n'y a pas de reservations pour ce logement.</p>
+                        <?php
+                    }
                     ?>
-                    <p>Il n'y a pas de reservations pour ce logement.</p>
-                    <?php
-                }
-                ?>
 
-            </div>
+                </div>
             </div>
 
             <?php
         }
     }
     ?>
-    </div>
+</div>
 
 </section>
