a74bca91440b5b42bd3287dc9360c71d9d747c1a
Elyroma <elynebeaudeau@orange.fr>
2022-05-22 18:34:44 +0200

    toujours

--- constant.php
+++ constant.php
@@ -15,7 +15,7 @@ $ORDER_FOR_OWNER = array(array("nom" => "Date", "value" => DATE_ORDER), array("n
 $ORDER_FOR_ASK = array(
     array("nom" => "Nombre de jour", "value" => "nb_day"), 
     array("nom" => "Date de début", "value" => "date_start"),
-    array("nom" => "note", "value" => "note"),
+    array("nom" => "note", "value" => "Note"),
     );
 
 //ne marche pas sur le serveur car le français n'est pas installé
